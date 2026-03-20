b0faff5f40a42f4f121cbaf0a086f8584ec60add
MaximilianHart <38765373+MaximilianHart@users.noreply.github.com>
2026-03-20 13:24:20 -0500

    feat: generate ability scores

--- main.py
+++ main.py
@@ -1,16 +1,179 @@
+import random
+
+
 def main():
-    print("Hello from dcc-de-chargen!")
+    print("Welcome to the DCC Dying Earth Character Generator!")
+
+    while True:
+        peasant = Character()
+        peasant.generate_zero_level()
+
+        print(f"\n--- {peasant.name or 'Unnamed Peasant'} ---")
+
+        print(
+            f"{'Strength:':13} {peasant.stats['Strength']} ({peasant.strength_mod:+})"
+        )
+        print(f"{'Agility:':13} {peasant.stats['Agility']} ({peasant.agility_mod:+})")
+        print(f"{'Stamina:':13} {peasant.stats['Stamina']} ({peasant.stamina_mod:+})")
+        print(
+            f"{'Intelligence:':13} {peasant.stats['Intelligence']} ({peasant.intelligence_mod:+})"
+        )
+        print(
+            f"{'Personality:':13} {peasant.stats['Personality']} ({peasant.personality_mod:+})"
+        )
+        print(f"{'Luck:':13} {peasant.stats['Luck']} ({peasant.luck_mod:+})")
+        print(f"AC:  {peasant.ac}")
+        print("-----------------------")
+
+        user_input = input(
+            "\nType r to repeat and create a new peasant, or q to finish..."
+        )
+
+        if user_input == "r":
+            print("\n" + "=" * 30 + "\n")
+            continue
+        else:
+            print("Good luck in the funnel, peasant!")
+            break
 
 
-if __name__ == "__main__":
-    main()
+def dice(number, size):
+    result = 0
+    for i in range(number):
+        die = random.randint(1, size)
+        result += die
+    return result
 
-# Pseudocode and logic
-# Dice roller
-#
-# Roll ability scores
-# Map ability scores to modifiers & wiz spells known and max spell level
-#
+
+class Character:
+    def __init__(self):
+        self.name = None
+        self.level = 0
+        self.stats = {
+            "Strength": 10,
+            "Agility": 10,
+            "Stamina": 10,
+            "Personality": 10,
+            "Intelligence": 10,
+            "Luck": 10,
+        }
+        self.armor_bonus = 0
+        self.max_hp = 0
+        self.occupation = None
+        self.alignment = None
+        self.speed = 0
+        self.initiative = 0
+        self.equipment = []
+        self.animus = {"Animus": None, "Animus Description": None}
+        self.birth_augur = None
+        self.lucky_roll = None
+        self.languages = []
+
+    @property
+    def strength_mod(self):
+        return self.calculate_modifier(self.stats["Strength"])
+
+    @property
+    def agility_mod(self):
+        return self.calculate_modifier(self.stats["Agility"])
+
+    @property
+    def stamina_mod(self):
+        return self.calculate_modifier(self.stats["Stamina"])
+
+    @property
+    def personality_mod(self):
+        return self.calculate_modifier(self.stats["Personality"])
+
+    @property
+    def intelligence_mod(self):
+        return self.calculate_modifier(self.stats["Intelligence"])
+
+    @property
+    def spells_memorized(self):
+        if self.stats["Intelligence"] <= 3:
+            return "No spellcasting possible"
+        elif self.stats["Intelligence"] <= 5:
+            return "-2 spells (minimum of 1 spell)"
+        elif self.stats["Intelligence"] <= 7:
+            return "-1 spell (minimum of 1 spell)"
+        elif self.stats["Intelligence"] <= 13:
+            return "No adjustment"
+        elif self.stats["Intelligence"] <= 16:
+            return "+1 spell"
+        elif self.stats["Intelligence"] <= 18:
+            return "+2 spells"
+        else:
+            return "+3 spells"
+
+    @property
+    def max_spell_level(self):
+        if self.stats["Intelligence"] <= 3:
+            return "No spellcasting possible"
+        elif self.stats["Intelligence"] <= 7:
+            return "1"
+        elif self.stats["Intelligence"] <= 9:
+            return "2"
+        elif self.stats["Intelligence"] <= 11:
+            return "3"
+        elif self.stats["Intelligence"] <= 14:
+            return "4"
+        else:
+            return "5"
+
+    @property
+    def luck_mod(self):
+        return self.calculate_modifier(self.stats["Luck"])
+
+    @property
+    def reflex_save(self):
+        return self.agility_mod
+
+    @property
+    def fortitude_save(self):
+        return self.stamina_mod
+
+    @property
+    def will_save(self):
+        return self.personality_mod
+
+    @property
+    def ac(self):
+        return 10 + self.agility_mod + self.armor_bonus
+
+    def calculate_modifier(self, score):
+        if score >= 20:
+            return 4
+        elif score >= 18:
+            return 3
+        elif score >= 16:
+            return 2
+        elif score >= 13:
+            return 1
+        elif score >= 9:
+            return 0
+        elif score >= 6:
+            return -1
+        elif score >= 4:
+            return -2
+        elif score >= 2:
+            return -3
+        else:
+            return -4
+
+    def roll_stats(self):
+        print("Rolling stats...")
+        for stat_name in self.stats:
+            self.stats[stat_name] = dice(3, 6)
+
+    def roll_hp(self):
+        pass
+
+    def generate_zero_level(self):
+        self.roll_stats()
+        self.roll_hp()
+
+
 # Determine birth auger and "lucky roll"
 #
 # Determine occupation, starting weapon, trade goods
@@ -30,3 +193,7 @@ if __name__ == "__main__":
 # Generate random name a la https://perchance.org/dying-earth-names#edit
 #
 # Print full character, possibly nicely formatted.
+#
+
+if __name__ == "__main__":
+    main()
