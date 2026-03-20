02fbc0ac1c9965b3fdcc71a1cac878ab0febce6a
MaximilianHart <38765373+MaximilianHart@users.noreply.github.com>
2026-03-20 09:41:14 -0500

    Initial commit with to-do list

---
+++ main.py
@@ -0,0 +1,32 @@
+def main():
+    print("Hello from dcc-de-chargen!")
+
+
+if __name__ == "__main__":
+    main()
+
+# Pseudocode and logic
+# Dice roller
+#
+# Roll ability scores
+# Map ability scores to modifiers & wiz spells known and max spell level
+#
+# Determine birth auger and "lucky roll"
+#
+# Determine occupation, starting weapon, trade goods
+#
+# Determine starting animus
+#
+# If vat-thing, determine pattern and starting flaw
+#
+# Determine languages
+#
+# Determine alignment
+#
+# Determine one random piece of equipment
+#
+# Roll on the Thaumaturgical Curious table
+#
+# Generate random name a la https://perchance.org/dying-earth-names#edit
+#
+# Print full character, possibly nicely formatted.
